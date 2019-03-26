0e478649bdadc09804c5dd0b0a76cf7e083f0115
niks3089 <niks3089@gmail.com>
2019-03-26 05:12:47 -0700

    Udpv6 integration test

--- test/integration/run_test.sh
+++ test/integration/run_test.sh
@@ -2,9 +2,10 @@
 set -e
 build_folder=$1
 timeout=$2
+test_folder=$3
 shift 2
 arguments=$@
 source ${build_folder}/activate.sh
 #sudo prior to timeout. in case its needed inside
 sudo echo "sudo trigger"
-timeout -s 2 $timeout python2 -u test.py $arguments || echo "Test timed out"; sleep 1
+timeout -s 2 $timeout python2 -u $test_folder/test.py $arguments || echo "Test timed out"; sleep 1
